ae1ec82624f7b10adb1387945420695112dfd51a
david-vargas98 <edgar981125@live.com.mx>
2023-10-05 19:13:52 -0600

    Se cambió hacia donde dirige la url del perfil, siendo a profile, y se cambia livewire de false a true para habilitarlo y poder usarlo.

--- config/adminlte.php
+++ config/adminlte.php
@@ -321,7 +321,7 @@ return [
         ['header' => 'account_settings'],
         [
             'text' => 'profile',
-            'url'  => 'admin/settings',
+            'url'  => 'profile',
             'icon' => 'fas fa-fw fa-user',
         ],
         [
@@ -537,5 +537,5 @@ return [
     |
     */
 
-    'livewire' => false,
+    'livewire' => true,
 ];
